0def6afabadeaed05a45307afc6277e2490f56cf
abbotteverett <severettabbott@gmail.com>
2018-04-11 16:58:55 -0700

    Updated instructions on homepage

--- src/components/HomePage/Homepage.js
+++ src/components/HomePage/Homepage.js
@@ -33,12 +33,14 @@ const HomePage = ({ bodyMargin }) => {
             <Header as='h2'>Quirks about the app</Header>
             <ul>
                 <li>This is a touch only app and is optimized for the phone. A mouse WIL NOT WORK. A pen with one of those rubber tipped styluses is preferred.</li>
+                <li>There is one flipbook per user. You will need to create a new user to make a new flipbook at this time.</li>
                 <li>You need internet in order to use the app (for image saving purposes)</li>
-                if the connection is slow, use wifi, it is a lot faster
+                <li>if the connection is slow, use wifi, it is a lot faster</li>
                 <li>Undo and Redo are only specific to your frame</li>
                 <li>When you toggle frames, it saves the last frame you were on to the database</li>
                 <li>Sometimes you have to wait for the frame count to update when you first log in to toggle</li>
                 <li>Make sure you save before you refresh the page or play the animation button</li>
+                <li>Frame rate is 12 frame per second</li>
                 <li>The brush size has to be a number between 1-99</li>
                 <li>Lightbox goes left and right given a number of frames upto 20</li>
                 <li>Don’t worry about little white outlines with the fill tool, the gif converter fixes them</li>
